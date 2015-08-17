bdb3cc8a47ef158efb8db924a127dbbe7da7602b
Tim Greenough <33067@cgs.act.edu.au>
2015-08-17 18:23:49 +1000

    Sections iOS application finished up to page 286; a plain table of names divided into sections based on the first letter of the name, with an index down the right side

--- Sections/Sections/ViewController.swift
+++ Sections/Sections/ViewController.swift
@@ -8,16 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let sectionsTableIdentifier = "SectionsTableIdentifier"
+    var names : [String : [String]]!
+    var keys : [String]!
+    @IBOutlet weak var tableView : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Register the default table view cell class that should be displayed for each row
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: sectionsTableIdentifier)
+        
+        let path = NSBundle.mainBundle().pathForResource("sortednames", ofType: "plist")
+        let namesDict = NSDictionary(contentsOfFile: path!)
+        // 'as' replaced with 'as!' due to error
+        names = namesDict as! [String : [String]]
+        keys = sorted(namesDict!.allKeys as! [String])
     }
+    
+    // MARK: Table View Data Source methods
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return keys.count
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let key = keys[section]
+        let nameSection = names[key]!
+        return nameSection.count
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return keys[section]
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(sectionsTableIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        
+        let key = keys[indexPath.section]
+        let nameSection = names[key]!
+        // 'textLabel' replaced wth 'textLabel!' due to error
+        cell.textLabel!.text = nameSection[indexPath.row]
+        
+        return cell
+    }
+    
+    // Can there be more items in the index that there are sections?
+    func sectionIndexTitlesForTableView(tableView: UITableView) -> [AnyObject]! {
+        return keys
     }
 
 
