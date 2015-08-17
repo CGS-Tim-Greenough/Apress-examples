b6f5b9a78c862038cf0f806ac4b7075e9864db3d
Tim Greenough <33067@cgs.act.edu.au>
2015-08-17 11:54:58 +1000

    Initial commit

---
+++ Sections/Sections/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sections
+//
+//  Created by Tim Greenough on 17/08/2015.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
