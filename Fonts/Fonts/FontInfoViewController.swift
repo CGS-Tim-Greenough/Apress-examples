05a921492bce65a705cc551be98142b7b2d87e97
Tim Greenough <33067@cgs.act.edu.au>
2015-09-08 21:21:01 +1000

    Some more textbook work on Fonts and stuff

---
+++ Fonts/Fonts/FontInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FontInfoViewController.swift
+//  Fonts
+//
+//  Created by Tim Greenough on 1/09/2015.
+//  Copyright (c) 2015 Apress. All rights reserved.
+//
+
+import UIKit
+
+class FontInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
