ddb4a1f97a3cfb80194f298722005a1283bfc481
Tim Greenough <33067@cgs.act.edu.au>
2015-09-08 22:06:00 +1000

    Up to page 439, Chapter 13

--- Persistence/Persistence/ViewController.swift
+++ Persistence/Persistence/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var lineFields : [UITextField]!
+    
+    func dataFilePath() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsDirectoryPath = paths[0] as! NSString
+        return documentsDirectoryPath.stringByAppendingPathComponent("data.plist") as String
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let filePath = self.dataFilePath()
+        if NSFileManager.defaultManager().fileExistsAtPath(filePath) {
+            let array = NSArray(contentsOfFile: filePath) as! [String]
+            for var i = 0; i < array.count; i++ {
+                lineFields[i].text = array[i]
+            }
+            
+            let app = UIApplication.sharedApplication()
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: "applicationWillResignActive:", name: UIApplicationWillResignActiveNotification, object: app)
+        }
+        
+        func applicationWillResignActive(notification : NSNotification) {
+            let filePath = self.dataFilePath()
+            let array = (self.lineFields as NSArray).valueForKey("text") as! NSArray
+            array.writeToFile(filePath, atomically: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
